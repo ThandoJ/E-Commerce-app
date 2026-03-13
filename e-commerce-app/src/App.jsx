e07eb44dbf5a64121fbf2b8de8f573345594bf4a
Thando Jasop <jasopthando0@gmail.com>
2026-03-13 12:40:39 +0200

    added tailwind.css

---
+++ e-commerce-app/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+ 
+export default function() {
+    return (
+        <div class="text-4xl text-black font-semibold">
+            Hello how are you 
+        </div>
+    );
+} 
