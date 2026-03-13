a71ccba48e5c0296235beedbaf9ecd93b3d04273
Thando Jasop <jasopthando0@gmail.com>
2026-03-13 12:57:24 +0200

    Added app.jsx

--- e-commerce-app/src/App.jsx
+++ e-commerce-app/src/App.jsx
@@ -1,9 +1,27 @@
+
 import React from "react";
- 
-export default function() {
-    return (
-        <div class="text-4xl text-black font-semibold">
-            Hello how are you 
-        </div>
-    );
-} 
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import Home from "./pages/Home"
+import ProductPage from "./pages/ProductPage"
+import CartPage from "./pages/CartPage"
+import Checkout from "./pages/Checkout"
+import OrderSuccess from "./pages/OrderSuccess"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<ProductPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/order-success" element={<OrderSuccess />} />
+
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
